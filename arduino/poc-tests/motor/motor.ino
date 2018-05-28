9719c405f3fa1adb25cba83a70bbe799329bb035
Christian Ashby <git@cashby.me.uk>
2018-05-28 11:47:53 +0100

    Motor test now works on all 8 faders)

--- arduino/poc-tests/motor/motor.ino
+++ arduino/poc-tests/motor/motor.ino
@@ -1,20 +1,48 @@
-int m1 = 6;           //Direction control pins
-int m2 = 7;           //Direction control pins
+const int MCTLA = 5;
+const int MCTLB = 6;
+const int FADERS = 8;
+const byte MApins[] = {7,8,9};
+const byte MBEN = 10;
 
 void setup(){
-  pinMode(m1,OUTPUT); //Arduino Pin#7 as Output 
-  pinMode(m2,OUTPUT); //Arduino Pin#8 as Output 
+  Serial.begin(9600);
+  pinMode(MCTLA,OUTPUT);
+  pinMode(MCTLB,OUTPUT);
+  pinMode(MBEN,OUTPUT);
+  digitalWrite(MBEN, HIGH);
+  for( int bt=0; bt<sizeof(MApins); bt++ ) {
+    pinMode(MApins[bt], OUTPUT);
+    Serial.print("pin ");
+    Serial.print(MApins[bt]);
+    Serial.print("=OUTPUT,");
+  }
+  Serial.println();
+  Serial.println("START");
+  
 }
 
 void loop(){
-  digitalWrite(m1,HIGH);   //Pin#7 as High
-  digitalWrite(m2,LOW);    //Pin#8 as Low
-  delay(400);               //Delay 4 seconds Motor rotates in clock wise direction
-  digitalWrite(m1,LOW);    //Pin#7 as Low
-  digitalWrite(m2,LOW);    //Pin#8 as Low 
-  digitalWrite(m2,HIGH);   //Pin#8 as High
-  digitalWrite(m1,LOW);    //Pin#7 as Low
-  delay(400);               //Delay for 4 seconds Motor rotates in anti clock wise direction
-  digitalWrite(m1,LOW);    //Pin#7 as Low
-  digitalWrite(m2,LOW);    //Pin#8 as Low
+  for( int fader=0; fader<FADERS; fader++ ) { 
+    Serial.print("Fader ");
+    Serial.println(fader);
+    for( int bt=0; bt<sizeof(MApins); bt++ ) {
+      byte state = bitRead(fader, bt);
+      digitalWrite(MApins[bt], state);
+      Serial.print("MA");
+      Serial.print(bt);
+      Serial.print("=");
+      Serial.println(state);
+    }
+    
+    digitalWrite(MCTLA,HIGH);   //Pin#7 as High
+    digitalWrite(MCTLB,LOW);    //Pin#8 as Low
+    delay(400);               //Delay 4 seconds Motor rotates in clock wise direction
+    digitalWrite(MCTLA,LOW);    //Pin#7 as Low
+    digitalWrite(MCTLB,LOW);    //Pin#8 as Low 
+    digitalWrite(MCTLB,HIGH);   //Pin#8 as High
+    digitalWrite(MCTLA,LOW);    //Pin#7 as Low
+    delay(400);               //Delay for 4 seconds Motor rotates in anti clock wise direction
+    digitalWrite(MCTLA,LOW);    //Pin#7 as Low
+    digitalWrite(MCTLB,LOW);    //Pin#8 as Low
+  }
 }
